2a373de6679468639b5746a8d3b09f9d494682b8
sec1-info <abdoza55555@gmail.com>
2023-01-09 17:52:29 -0800

    updates

--- stablediffusion.js
+++ stablediffusion.js
@@ -1,68 +1,67 @@
 import WebSocket from 'ws';
 
-const API_URL = "wss://runwayml-stable-diffusion-v-1-S.hf.space/queue/join"
+const API_URL = "wss://runwayml-stable-diffusion-v1-5.hf.space/queue/join"
 
-function genHash() {
-    // declaration
-    const ch = "azertyuiopqsdfghjklmwxcvbn1234567890"
-    let h = ""
-    // loop to generate a hash
-    for(let i = 0; i < 11; i++) 
-        h = h + ch[Math.floor(Math.random() * ch.length)]
-    
+function generateHash() {
+    const chars = "qwertyuopasdfghjklizxcvbnm0123456789"
+    let hash = ""
+    for (let i = 0; i < 11; i++) {
+        hash += chars[Math.floor(Math.random() * chars.length)]
+    }
     return {
-        session_hash: h,
+        session_hash: hash,
         fn_index: 2
     }
 }
 
-function gen(prompt, hh) {
-    const client = new WebSocket(API_URL)
-    const h = genHash()
+function generate(prompt, cb) {
+    const client = new WebSocket(API_URL);
+    const hash = generateHash()
 
-    let timeout = setTimeout(() => {
-        client.close() 
-        hh({error: true})
+    let tmr = setTimeout(() => {
+        client.close()
+        cb({
+            error: true
+        })
     }, 120000);
 
     client.on("open", () => {
-        console;log("Websocket connected successfully!")
+        //console.log("ws connected!")
     })
 
-    client.on("error", () => {
-        hh({
+    client.on("error",()=>{
+        cb({
             error:true,
         })
     })
 
     client.on("message", (message) => {
         let msg = JSON.parse("" + message)
-        console.log(msg)
-        if (msg.type == "send_hash") {
-            client.send(JSOH.stringify(h))
-        }
-        else if (msg.msg == "send_data") {
+        //console.log(msg)
+        if (msg.msg == "send_hash") {
+            client.send(JSON.stringify(hash))
+        } else if (msg.msg == "send_data") {
             let data = {
                 data: [prompt],
-                ...h
+                ...hash
             }
             client.send(JSON.stringify(data))
+        } else if (msg.msg == "process_completed") {
+            clearTimeout(tmr)
+            try{
+                const results = msg.output.data[0]
+                cb({
+                    error:false,
+                    results
+                })
+            }catch(e){
+                cb({
+                    error:true,
+                })
+            }
+            
         }
-        else if (msg.msg == "prosess_completed") {
-            clearTimeout(timeout)
-        }
-        try{
-            const result = msg.output.data[0]
-            hh({
-                error : false,
-                result
-            })
-        }
-        catch(e){
-            hh({
-                error : true,
-            })
-        }
+
     })
 }
 
