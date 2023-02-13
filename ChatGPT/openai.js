b7ce080e53835e1f0b66f9f57dd7c363e705a160
sec1-info <abdoza55555@gmail.com>
2023-02-13 15:42:03 +0100

    AI Bot

--- ChatGPT/openai.js
+++ ChatGPT/openai.js
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv'
 import {ChatGPTAPIBrowser} from 'chatgpt'
-import {Client, GatewayIntentBits, REST, Routes, Partials, ChannelType, AttachmentBuilder } from 'discord.js';
+import { Client, GatewayIntentBits, REST, Routes, Partials, ChannelType, AttachmentBuilder } from 'discord.js'
 import Conversations from '../ChatGPT/conversation.js'
-import stableDiffusion from '../ChatGPT/stablediffusion.js';
 
 const MRCL = process.env.MAX_RESPONSE_CHUNK_LENGTH
 dotenv.config()
@@ -11,7 +10,7 @@ async function initChatGPT() {
     const api = new ChatGPTAPIBrowser({
         email: process.env.OPENAI_EMAIL,
         password: process.env.OPENAI_PASSWORD
-    })  //ChatGPT login method (disabled until we fix ChatGPTAPI issues)
+    })  //ChatGPT login method (disable it if you get trouble with ChatGPT API)
 
     /*const api = new ChatGPTAPI({
         apiKey: process.env.OPENAI_API_KEY
@@ -126,57 +125,18 @@ export async function aifr() {
                 message.reply("Ghellab Abderrahmane (Rhaym), Computer Sciences student at University of Boumerdes Faculty of Sciences, Apps/Games developer (Windows/MacOS/iOS), Gamer, frontend web dev and UI/UX designer developed this bot")
                 return;
             }
-            
-            if (message.content.toLowerCase() == "who developed this bot?") {
-                await message.channel.sendTyping();
-                message.reply("Ghellab Abderrahmane (Rhaym), Computer Sciences student at University of Boumerdes Faculty of Sciences, Apps/Games developer (Windows/MacOS/iOS), Gamer, frontend web dev and UI/UX designer developed this bot")
-                return;
-            }
-
-            if (message.content.toLowerCase().includes("generate image:")) {
-                const prompt = message.content.substring(15)
-                try {
-                    await message.channel.sendTyping();
-                    stableDiffusion.generate(prompt, async (result) => {
-                        if (result.error) {
-                            await message.reply("Error happened while generating image...")
-                            return;
-                        }
-                        try {
-                            const attachments = []
-                            for (let i = 0; i < result.results.length; i++) {
-                                let data = result.results[i].split(",")[1]
-                                const buffer = Buffer.from(data, "base64")
-                                let attachment = new AttachmentBuilder(buffer, `result-${i}.jpg`)
-                                attachments.push(attachment)
-                            }
-                            await message.reply({content: "Done", files: [attachments] })
-                        } catch (e) {
-                            console.error(e)
-                            await message.reply("Error happened while attaching image...")
-                        }
-            
-                    })
-                } catch (e) {
-                    console.error(e)
-                }
-                return;
-            }
-            
 
             let conversationInfo = Conversations.getConversation(user.id)
             try {
                 await message.channel.sendTyping();
-                setTimeout(async () => {
-                  askQuestion(message.content, async (response) => {
-                      if (response.length >= MRCL) {
-                          const attachment = new AttachmentBuilder(Buffer.from(content, 'utf-8'), { name: 'response.txt' });
-                          await message.reply(`Sorry, but response is longer than discord message length limit, so I've attached the response as a txt file.`, { files: [attachment] });
-                      } else {
-                          await message.reply(response)
-                      }
-                  }, { conversationInfo });
-                }, 5000); // pause execution for 5000 milliseconds (5 seconds)
+                askQuestion(message.content, async (response) => {
+                    if (response.length >= MRCL) {
+                        const attachment = new AttachmentBuilder(Buffer.from(content, 'utf-8'), { name: 'response.txt' });
+                        await message.reply(`Sorry, but response is longer than discord message length limit, so I've attached the response as a txt file.`, { files: [attachment] });
+                    } else {
+                        await message.reply(response)
+                    }
+                }, { conversationInfo })
             } catch (e) {
                 console.error(e)
             }
@@ -185,35 +145,6 @@ export async function aifr() {
             return;
         }
     })
-
- /*   async function handle_interaction_image(interaction) {
-        const prompt = interaction.options.getString("prompt")
-        try {
-            await interaction.deferReply()
-            stableDiffusion.generate(prompt, async (result) => {
-                if (result.error) {
-                    await interaction.editReply({ content: "error..." })
-                    return;
-                }
-                try {
-                    const attachments = []
-                    for (let i = 0; i < result.results.length; i++) {
-                        let data = result.results[i].split(",")[1]
-                        const buffer = Buffer.from(data, "base64")
-                        let attachment = new AttachmentBuilder(buffer, { name: "result0.jpg" })
-                        attachments.push(attachment)
-                    }
-                    await interaction.editReply({ content: "done...", files: attachments })
-                } catch (e) {
-                    await interaction.editReply({ content: "error..." })
-                }
-
-            })
-        } catch (e) {
-            console.error(e)
-        }
-    }*/
-    
 
     client.login(process.env.DISCORD_BOT_TOKEN);
 }
--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # AI-DiscordBot
-An adorable multipurpose Discord Bot which has Info, Moderation, Fun Commands.
+An adorable ChatBot powered by GPT-3.5 AI language model.
 Built for GDSC
-Made with love by [@rhaym-tech](https://github.com/rhaym-tech) and [@akhdari](https://github.com/akhdari)
+Made with love by [@rhaym-tech](https://github.com/rhaym-tech)
 
 # Credits
 Google Developers Students Club - University of Boumerdes
 
-[@rhaym-tech](https://github.com/rhaym-tech): project owner
-
-[@akhdari](https://github.com/akhdari): project owner
+[@rhaym-tech](https://github.com/rhaym-tech): AI ChatBot developer
 
 [@transitive-bullshit](https://github.com/transitive-bullshit): Node.js client for ChatGPT developer
 
