b888025e5fbd759dae100d01d50604ff5b9c008a
sec1-info <abdoza55555@gmail.com>
2023-03-10 09:36:42 +0100

    add profanity filter

--- src/commands/fun/say.js
+++ src/commands/fun/say.js
@@ -1,4 +1,6 @@
 const { ApplicationCommandOptionType } = require("discord.js");
+var Filter = require('bad-words'),
+    filter = new Filter();
 
 /**
  * @type {import("@structures/Command")}
@@ -24,15 +26,23 @@ module.exports = {
         },
       ],
     },
+
     async messageRun(message, args) {
         const text = args.join(" ");
         if (!text) {
-          return message.reply("Please provide some text to say!");
+            return message.reply("Please provide some text to say!");
         }
-        await message.channel.send(text);
-      },
+        if (filter.isProfane(text)) {
+            console.log(filter.isProfane(text));
+            return message.reply("I'm sorry, but I cannot say offensive or inappropriate words, including the bad word you provided. My purpose is to assist and provide helpful features to users in a professional and respectful manner. Let's keep the conversation positive and constructive.");
+       }
+        await message.channel.send(filter.clean(text));
+    },
       async interactionRun(interaction) {
         const text = interaction.options.getString("input");
+        if (filter.isProfane(text)) {
+          return interaction.followUp("I'm sorry, but I cannot say offensive or inappropriate words, including the bad word you provided. My purpose is to assist and provide helpful features to users in a professional and respectful manner. Let's keep the conversation positive and constructive.");
+        }
         await interaction.followUp(text);
       },
   };
