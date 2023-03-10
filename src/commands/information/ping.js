80a30f1b4058629920cc786b79a6b6684ec6e155
sec1-info <abdoza55555@gmail.com>
2023-03-10 21:22:21 +0100

    improve ping

--- src/commands/information/ping.js
+++ src/commands/information/ping.js
@@ -22,14 +22,14 @@ module.exports = {
   async messageRun(message) {
     const embed = new EmbedBuilder()
     .setTitle("🏓 Pong 🏓.")
-    .setDescription(`**Bot's latency:**'\'${message.createdTimestamp - Date.now()}\'ms. \n**API's latency:** \`${Math.floor(message.client.ws.ping)}\`ms \n**Database's latency:** \`${Math.floor(Math.random() * 35 + 35)}\`ms`);
+    .setDescription(`**Bot's latency:** '\'${Date.now() + message.createdTimestamp}ms.\' \n**API's latency:** \`${Math.floor(message.client.ws.ping)}ms.\` \n**Database's latency:** \`${Math.floor(Math.random() * 35 + 35)}\`ms`);
     await message.reply({ embeds: [embed] });
   },
 
   async interactionRun(interaction) {
     const embed = new EmbedBuilder()
     .setTitle("🏓 Pong 🏓.")
-    .setDescription(`**Bot's latency:**'\'${interaction.createdTimestamp - Date.now()}\'ms. \n**API's latency:** \`${Math.floor(interaction.client.ws.ping)}\`ms \n**Database's latency:** \`${Math.floor(Math.random() * 35 + 35)}\`ms`);
+    .setDescription(`**Bot's latency:** '\'${Date.now() - interaction.createdTimestamp}\'ms. \n**API's latency:** \`${Math.floor(interaction.client.ws.ping)}ms.\` \n**Database's latency:** \`${Math.floor(Math.random() * 35 + 35)}ms.\``);
     await interaction.followUp({ embeds: [embed] });
   },
 };
